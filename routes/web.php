6dd689a61c7b6565089d9fc058076f1d9f78b53d
Gabriel Teixeira de Carvalho <70416214+GabrielT31xeira@users.noreply.github.com>
2021-04-21 12:57:31 -0300

    usando o Auth

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
 use App\Http\Controllers\PagesController;
+use App\Http\Controllers\Register\RegisterController;
+use App\Http\Controllers\Login\LoginController;
+use App\Http\Controllers\Adm\AdminController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -18,5 +24,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('login',[PagesController::class, 'login']);
-Route::get('/register',[PagesController::class, 'register']);
+Auth::routes();
+
+Route::get('/admin/dashboard',[AdminController::class, 'index']);
+// Route::group(['prefix' => 'Adm','middleware' => 'auth'],function(){
+    // });
+    
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    
+    // Route::get('/login',[PagesController::class, 'login']);
+    // Route::get('/register',[PagesController::class, 'register']);
+    
+    // Route::post('/login',[LoginController::class, 'login'])->name('login');
+    // Route::post('/register',[RegisterController::class, 'createUser'])->name('register');
--- webpack.mix.js
+++ webpack.mix.js
@@ -6,25 +6,11 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
+ | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
  */
 
-//  CSS
-mix.styles([
-    'resources/views/css/welcome/reset.css',
-    'resources/views/css/welcome/index.css'
-], 'public/css/welcome/index.css');
-
-mix.styles([
-    'resources/views/css/login/reset.css',
-    'resources/views/css/messages/error.css',
-    'resources/views/css/login/index.css'
-], 'public/css/login/index.css');
-
-mix.styles([
-    'resources/views/css/register/reset.css',
-    'resources/views/css/messages/error.css',
-    'resources/views/css/register/index.css'
-], 'public/css/register/index.css');
+mix.js('resources/js/app.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css')
+    .sourceMaps();
