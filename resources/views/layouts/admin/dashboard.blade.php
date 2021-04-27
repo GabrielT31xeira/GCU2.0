0bc787a108ec572dcc7c7a78a38f05edd171a2c2
Gabriel Teixeira de Carvalho <70416214+GabrielT31xeira@users.noreply.github.com>
2021-04-27 10:53:07 -0300

    Mostrando usuarios e pesquisa de usuario

--- resources/views/layouts/admin/dashboard.blade.php
+++ resources/views/layouts/admin/dashboard.blade.php
@@ -6,6 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="/css/admDashboard/index.css" rel="stylesheet">
+    <script src="{{ asset('js/app.js') }}"></script>
+
     @livewireStyles
 
     <title>GCU :: Welcome</title>
@@ -15,9 +17,13 @@
 
     <div class="navbar-container">
         <p class="navbar-logo">GCU </p>
-        <livewire:logout-link /> 
+        <a href="/admin/dashboard/allUsers">Ver Usuários</a>
+        <livewire:logout-link />
     </div>
 
+    <div>
+        @yield('content')
+    </div>
 
     <footer class="footer">
         <p class="footer-p">GCU &copy; 2021</p>
